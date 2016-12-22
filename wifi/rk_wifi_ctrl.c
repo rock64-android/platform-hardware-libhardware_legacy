c31139edb13ef24d42ceca25191b5ec2f656d2e5
zzc <zzc@rock-chips.com>
2016-12-22 10:37:17 +0800

    WIFI AP6330:does not allow android to set fw path
    Change-Id: I2e4d91d8531f2f07dd0822aafd37e464fcea3cae
    Signed-off-by: zzc <zzc@rock-chips.com>

--- wifi/rk_wifi_ctrl.c
+++ wifi/rk_wifi_ctrl.c
@@ -158,6 +158,11 @@ int check_wifi_chip_type(void)
 	wifi_chip_type = RTL8723CS;
 	ALOGD("Read wifi chip type OK ! wifi_chip_type = RTL8723CS");
     }
+    else if (0 == strncmp(buf, "AP6330", strlen("AP6330")))
+    {
+        wifi_chip_type = AP6330;
+	ALOGD("Read wifi chip type OK ! wifi_chip_type = AP6330");
+    }
 
 done:
     return wifi_chip_type;
